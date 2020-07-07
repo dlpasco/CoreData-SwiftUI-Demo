9026705961de0e6317bababb8ee009f539caac15
Daniel Pasco <daniel@blackpixel.com>
2020-07-07 11:27:37 -0700

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Daniel Pasco on 7/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Core_Data_Super_Ultra_Fun_FestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_Data_Super_Ultra_Fun_FestApp.swift
+//  Shared
+//
+//  Created by Daniel Pasco on 7/7/20.
+//
+
+import SwiftUI
+
+@main
+struct Core_Data_Super_Ultra_Fun_FestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
